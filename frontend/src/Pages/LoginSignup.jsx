1c0b4ae193f234203ea98ec084378df9aab3c00b
JashanpreetSingh9891 <js5137311@gmail.com>
2025-07-13 12:42:57 +0530

    ✨ Added stylish checkbox component

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import "../CSS/LoginSignup.css"
+import React from "react";
+import "../CSS/LoginSignup.css";
 const LoginSignup = () => {
   return (
-    <div className='loginsignup'>
+    <div className="loginsignup">
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
-        <div className='loginsignup-fields'>
-          <input type='text' placeholder='Your Name'/>
-          <input type='email' placeholder='Email Address'/>
-          <input type='password' placeholder='Password'/>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
           <button>Continue</button>
         </div>
-        
+
         <p className="loginsignup-login">
           Already have an account ? <span>Login here</span>
         </p>
-        <div className='loginsignup-agree'>
+        <label
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            cursor: "pointer",
+            fontSize: "16px",
+            padding: "8px 12px",
+            borderRadius: "8px",
+            backgroundColor: checked ? "#d1e7dd" : "#f8f9fa",
+            border: "1px solid #ced4da",
+            transition: "all 0.2s ease",
+            userSelect: "none",
+          }}
+        >
+          <input
+            type="checkbox"
+            checked={checked}
+            onChange={handleChange}
+            style={{
+              width: "18px",
+              height: "18px",
+              marginRight: "10px",
+              accentColor: "#198754", // green color
+            }}
+          />
+          Accept Terms & Conditions
+        </label>
+        {/* <div className='loginsignup-agree'>
           <input type='checkbox' name='' id=''/>
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
-        </div>
+        </div> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
